4abf2f2fbba5b2d16e0bd5d39c647bcbd3aaa1ba
grouls <47697948+grouls@users.noreply.github.com>
2019-10-19 14:11:06 +0100

    simple useEffect demo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-
+import users from "./users";
 import "./styles.css";
 
 function App() {
+  const [user, setUser] = useState("guest");
+
+  useEffect(() => {
+    document.title = `Welcome ${user}!`;
+  });
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <select onChange={e => setUser(e.target.value)}>
+        {users.map(user => (
+          <option key={user.id} value={user.name}>
+            {user.name}
+          </option>
+        ))}
+      </select>
+      <p>user is {user}</p>
     </div>
   );
 }
---
+++ src/users.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    name: "guest"
+  },
+  {
+    id: 2,
+    name: "gary"
+  },
+  {
+    id: 3,
+    name: "michael"
+  },
+  {
+    id: 4,
+    name: "john"
+  },
+  {
+    id: 5,
+    name: "stephen"
+  }
+];
